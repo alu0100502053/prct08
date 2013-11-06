65a3d9d40f5842e122f98ccf985ce7ca2c3b82e9
Killian Jimenez <alu0100502053@ull.edu.es>
2013-11-06 12:58:14 +0000

    Creada la expectativa de que debe ser cuadrada la matriz.
    Failures:
    
      1) Matriz Debe comprobar que la matriz es cuadrada
         Failure/Error: expect { Matriz.new([2,3,3],[3,4],[1,2,3]) }.to raise_error
           expected Exception but nothing was raised
         # ./spec/matrices_spec.rb:8:in `block (2 levels) in <top (required)>'
    
    Finished in 0.00105 seconds
    1 example, 1 failure
    
    Failed examples:
    
    rspec ./spec/matrices_spec.rb:7 # Matriz Debe comprobar que la matriz es cuadrada

--- lib/matrices.rb
+++ lib/matrices.rb
@@ -1,2 +1,6 @@
-class Matrices
+class Matriz
+	def initialize(*args)  #args recogerá en un array las filas de nuestra matriz.
+		@matriz = Array.new()
+		@dimension = args.size  #Guardamos en una variable de instancia la dimension de la matriz.
+	end
 end
--- spec/matrices_spec.rb
+++ spec/matrices_spec.rb
@@ -1,7 +1,11 @@
 require 'matrices'
 
-describe Matrices do
+describe Matriz do
 	before :each do
-		# @matriz = Matrices.new([2,3,4],[5,6,4],[2,3,3])
+		@matriz = Matriz.new([1,2,3],[4,5,6],[7,8,9])
+	end
+	it "Debe comprobar que la matriz es cuadrada" do
+		expect { Matriz.new([2,3,3],[3,4],[1,2,3]) }.to raise_error
+		expect { Matriz.new([2,3,3],[1,2,3]) }.to raise_error
 	end
 end
