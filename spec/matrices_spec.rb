5f27be1754bb57b003ed44ad0970a5eb32a38f35
Killian Jimenez <alu0100502053@ull.edu.es>
2013-11-06 13:06:12 +0000

    Creada expectativa para la resta de dos matrices.
    Failures:
    
      1) Matriz Debe restar dos matrices
         Failure/Error: @matriz3 = @matriz-@matriz2
         NoMethodError:
           undefined method `-' for #<Matriz:0x00000000fde1c0>
         # ./spec/matrices_spec.rb:39:in `block (2 levels) in <top (required)>'
    
    Finished in 0.01531 seconds
    4 examples, 1 failure
    
    Failed examples:
    
    rspec ./spec/matrices_spec.rb:38 # Matriz Debe restar dos matrices

--- spec/matrices_spec.rb
+++ spec/matrices_spec.rb
@@ -35,4 +35,17 @@ describe Matriz do
 		@matriz3[2][2].should eq(16)
 	end
 
+	it "Debe restar dos matrices" do
+		@matriz3 = @matriz-@matriz2
+		@matriz3[0][0].should eq(-2)
+		@matriz3[0][1].should eq(0)
+		@matriz3[0][2].should eq(2)
+		@matriz3[1][0].should eq(-2)
+		@matriz3[1][1].should eq(0)
+		@matriz3[1][2].should eq(2)
+		@matriz3[2][0].should eq(-2)
+		@matriz3[2][1].should eq(0)
+		@matriz3[2][2].should eq(2)
+	end
+
 end
